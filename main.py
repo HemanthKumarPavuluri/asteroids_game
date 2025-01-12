97b507522c66cbb69191dd0c3b8ff9eaad3fb72c
HemanthKumarPavuluri <phk.phk007@gmail.com>
2025-01-12 11:00:38 -0600

    circleshape and player classes added, with roation to player

--- main.py
+++ main.py
@@ -3,19 +3,36 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
+
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print("Starting asteroids!")
+    clock = pygame.time.Clock()
+
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                return
         # Fill the screen with a solid "black" color (RGB: 0, 0, 0)
         screen.fill((0, 0, 0))
-
+        # handles rotation
+        player.update(dt) 
+        #Draws player triangle to screen
+        player.draw(screen)
         # Refresh the display to show the updates
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
+
+        
 
 
 if __name__ == "__main__":
