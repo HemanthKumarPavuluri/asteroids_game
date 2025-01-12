467fe82d6d87827e18fbdfa480c003ad3a4e640c
HemanthKumarPavuluri <phk.phk007@gmail.com>
2025-01-12 00:14:47 -0600

    pygame window added

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 # this allows us to use code from
 # the open-source pygame library
 # throughout this file
-from constants import *
 import pygame
+from constants import *
+
 
 def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    while True:
+        # Fill the screen with a solid "black" color (RGB: 0, 0, 0)
+        screen.fill((0, 0, 0))
+
+        # Refresh the display to show the updates
+        pygame.display.flip()
+
 
 if __name__ == "__main__":
     main()
